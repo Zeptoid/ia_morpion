e0397a25a0ed057f630de0fdcb75ddc2b1d60d9c
zeptoid <ouldyoucef22@gmail.com>
2021-12-30 15:51:00 +0100

    feat:fonction de base

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ typedef struct
 typedef struct 
 {
     uint64_t nb;
-    liste lb;
+    liste liste_billes;
 
 }boite;
 
@@ -55,55 +55,7 @@ void codage_grille(uint8_t grille[3][3],uint64_t nb){
 
 }
 
-typedef struct bille
-{
-    uint16_t pos;
-    struct bille next;
-}bille;
 
-typedef struct
-{
-    uint64_t taille;
-    bille * head;
-}liste;
-
-typedef struct 
-{
-    uint64_t nb;
-    liste lb;
-
-}boite;
-
-uint64_t codage_entier( uint8_t grille[3][3])
-{
-    uint8_t i,j,p=6561;
-    uint64_t nb=0;
-
-    for(i=0;i<3 ;i++){
-        for(j=0;j<3;j++){
-            nb=nb+grille[i][j]*p;
-            p=p/3;
-        }
-    }
-    return nb;
-}
-
-void codage_grille(uint8_t grille[3][3],uint64_t nb){
-    uint8_t i,j;
-    for(i=0;i<3 ;i++){
-        for(j=0;j<3;j++){
-            grille[i][j]=nb%3;
-            nb=nb/3;
-        }
-    }
-
-}
-
-int main() {
-
-
-    return EXIT_SUCCESS;
-}
 
 
 
