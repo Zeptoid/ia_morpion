98f919cb0ebc30882ee749b827937816351fc226
zeptoid <ouldyoucef22@gmail.com>
2021-12-30 15:42:38 +0100

    feat: structure et fonction de codage

--- test.c
+++ test.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
 #include <stdlib.h>
@@ -42,21 +39,11 @@ uint64_t codage_entier( uint8_t grille[3][3])
 
 void codage_grille(uint8_t grille[3][3],uint64_t nb){
     uint8_t i,j;
-    for(i=3;i>=0 ;i--){
-        for(j=3;j>=3;j--){
+    for(i=0;i<3 ;i++){
+        for(j=0;j<3;j++){
             grille[i][j]=nb%3;
             nb=nb/3;
         }
     }
 
 }
-
-int main() {
-
-
-    return EXIT_SUCCESS;
-}
-
-
-
-
